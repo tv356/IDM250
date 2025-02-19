0992c700d91acbc86496763ed045438cf923f1e8
Tran (Cheryl) Vo <71985185+tv356@users.noreply.github.com>
2025-02-19 13:22:35 +0700

    Create 404.php

---
+++ bakery/404.php
@@ -0,0 +1,17 @@
+<?php get_header();?>
+
+    <div class="container">
+        <div class="row">
+            <div class="page_404">
+                <div class="box">
+                    <h3>4</h3>
+                    <img src="<?php bloginfo('stylesheet_directory') ?>/assets/images/404.png" alt="">
+                    <h3>4</h3>
+                </div>
+                <p>OOPPS! THE PAGE YOU WERE LOOKING FOR, COULDN'T BE FOUND.</p>
+                <span>Try the search below to find matching pages:</span>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
