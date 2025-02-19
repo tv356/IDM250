f8804365fd5ff795cc67ac6b9103452f4024330d
Tran (Cheryl) Vo <71985185+tv356@users.noreply.github.com>
2025-02-19 13:22:14 +0700

    Create footer.php

---
+++ bakery/footer.php
@@ -0,0 +1,23 @@
+<footer>
+        <div class="box_footer">
+            <div class="footer_title"><h3>For your days</h3></div>
+            <div class="footer_menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">menu</a></li>
+                    <li><a href="#">CAMPAIGNS</a></li>
+                    <li><a href="#">BLIND BOXES</a></li>
+                </ul>
+            </div>
+            <div class="copy_right">
+                <p>copyright © 2023. all rights reserved</p>
+            </div>
+        </div>
+</footer>
+
+<script src="<?php bloginfo('stylesheet_directory') ?>/assets/jquery-3.7.1.min.js"></script>
+<script src="<?php bloginfo('stylesheet_directory') ?>/assets/bootstrap-5/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('stylesheet_directory') ?>/libs/slick/slick/slick.min.js"></script>
+<script src="<?php bloginfo('stylesheet_directory') ?>/assets/js/js.js"></script>
+</body>    
+</html>
