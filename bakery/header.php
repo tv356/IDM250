85d8623de1933dcd4a8b588de4015005fd2b72ef
Tran (Cheryl) Vo <71985185+tv356@users.noreply.github.com>
2025-03-22 04:34:17 +0700

    Update header.php

--- bakery/header.php
+++ bakery/header.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="https://storage.googleapis.com/atlas-core-active-storage/pun-im/favicon.png" type="image/png">
+
     <link href="https://fonts.googleapis.com/css2?family=Chewy&family=Forum&family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Single+Day&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory') ?>/assets/reset.css" />
